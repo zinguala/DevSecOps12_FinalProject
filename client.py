4fce05b3337df4ab80e238b8e82e108d5f95aa2f
Hai Hatan <moshe.hai.hatan94@gmail.ocm>
2023-07-12 15:41:53 +0300

    add main function to client and rename to remove unseen space

---
+++ client.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+def ping():
+
+    data = {'body' : 'ping'}
+
+    r = requests.post('http://127.0.0.1:5005',json = data)
+    ans = r.json()
+
+    print(ans['body'])
+
+if __name__ == "__main__":
+       ping()
