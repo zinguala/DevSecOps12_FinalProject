9f992258250c1d07d731ced2e66fd6b7c9e4fac5
zinguala <34973070+zinguala@users.noreply.github.com>
2023-08-14 22:13:28 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,15 +2,9 @@ import requests
 import json
 import os
 
-def ping():
+data = {'body' : 'ping'}
 
-    data = {'body' : 'ping'}
+r = requests.get('http://localhost:5005',json = data)
+ans = r.json()
 
-    r = requests.get('http://localhost:5005',json = data)
-    ans = r.json()
-
-
-    print(ans['body'])
-
-if __name__ == "__main__":
-       ping()
+print(ans['body'])
