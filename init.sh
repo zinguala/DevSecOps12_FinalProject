9d3219321130b202245bd1df56c1e494758eb0a7
moshe hai ahatan <122904809+HaiHatan@users.noreply.github.com>
2023-08-20 19:51:47 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -62,17 +62,3 @@ sudo echo -e "\nstream {\n\tserver {\n\t\tlisten $vm_ip:5005;\n\t\t#TCP traffic 
 sudo chmod 644 /etc/nginx/nginx.conf
 sudo systemctl restart nginx
 sudo ufw allow 5005
-sudo apt install openjdk-17-jre -y
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins -y
-sudo apt install git -y
-sudo apt install pipx -y
-pipx install --include-deps ansible
-sudo apt install openssh-client
-sudo ssh-keygen
-sudo ssh-copy-id -i /root/.ssh
