8a010bd8b73d702a733045e7eebb4d2381ebe93c
zinguala <34973070+zinguala@users.noreply.github.com>
2023-09-07 23:15:40 +0300

    Update Init ansible jenkins machine.sh

--- Init ansible jenkins machine.sh
+++ Init ansible jenkins machine.sh
@@ -11,6 +11,7 @@ curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
 sudo apt-get install jenkins -y
 sudo apt install git -y
 sudo apt install pipx -y
